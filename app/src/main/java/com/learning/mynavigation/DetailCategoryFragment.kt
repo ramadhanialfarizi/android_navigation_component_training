fad6448e64eeedb119afbcf08cd2160905a48d26
ramadhanialfarizi <ramadhani.alfarizi@gmail.com>
2025-09-03 22:40:36 +0700

    Codelab Kirim Data dengan Bundle

--- app/src/main/java/com/learning/mynavigation/DetailCategoryFragment.kt
+++ app/src/main/java/com/learning/mynavigation/DetailCategoryFragment.kt
@@ -1,12 +1,12 @@
 package com.learning.mynavigation
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.findNavController
-import com.learning.mynavigation.databinding.FragmentCategoryBinding
+import com.learning.mynavigation.databinding.FragmentDetailCategoryBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -15,18 +15,13 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [CategoryFragment.newInstance] factory method to
+ * Use the [DetailCategoryFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class CategoryFragment : Fragment() {
+class DetailCategoryFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var _binding: FragmentCategoryBinding? = null
-    private val binding get() = _binding !!
-
-    companion object {
-        val EXTRA_NAME = "extra_name"
-        val EXTRA_STOCK = "extra_stock"
-    }
+    private var _binding: FragmentDetailCategoryBinding? = null
+    private val binding get() = _binding!!
 
 
     override fun onCreateView(
@@ -34,21 +29,19 @@ class CategoryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_category, container, false)
-        _binding = FragmentCategoryBinding.inflate(inflater, container, false)
+        _binding = FragmentDetailCategoryBinding.inflate(inflater, container, false)
         val view = binding.root
-        return view
-    }
+        return view    }
 
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btnCategoryLifestyle.setOnClickListener { view ->
-            val mBundle = Bundle()
-            mBundle.putString(EXTRA_NAME, "Lifestyle")
-            mBundle.putLong(EXTRA_STOCK, 7)
-            view.findNavController().navigate(R.id.action_categoryFragment_to_detailCategoryFragment, mBundle)
-        }
+        val dataName = arguments?.getString(CategoryFragment.EXTRA_NAME)
+        val dataDescription = arguments?.getLong(CategoryFragment.EXTRA_STOCK)
+
+        binding.tvCategoryName.text = dataName
+        binding.tvCategoryDescription.text = "Stock : $dataDescription"
     }
 
     override fun onDestroy() {
