c5115769fd9595a20cd2d49aa90850e5281542a9
ramadhanialfarizi <ramadhani.alfarizi@gmail.com>
2025-09-03 22:19:19 +0700

    Codelab Navigation Component

---
+++ app/src/main/java/com/learning/mynavigation/HomeFragment.kt
@@ -0,0 +1,58 @@
+package com.learning.mynavigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import com.learning.mynavigation.databinding.FragmentHomeBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HomeFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnCategory.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_homeFragment_to_categoryFragment2)
+        )
+
+//        binding.btnCategory.setOnClickListener { view ->
+//            view.findNavController().navigate(R.id.action_homeFragment_to_categoryFragment2)
+//        }
+
+        binding.btnProfile.setOnClickListener { view ->
+            view.findNavController().navigate(R.id.action_homeFragment_to_profileActivity2)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
