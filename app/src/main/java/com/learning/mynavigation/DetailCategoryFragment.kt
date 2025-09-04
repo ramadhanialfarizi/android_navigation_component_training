49a4fefd46eeb1f4c007602448ac2ca3c4950de0
ramadhanialfarizi <ramadhani.alfarizi@gmail.com>
2025-09-04 23:18:27 +0700

    Codelab Pop Up Behaviour

--- app/src/main/java/com/learning/mynavigation/DetailCategoryFragment.kt
+++ app/src/main/java/com/learning/mynavigation/DetailCategoryFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.learning.mynavigation.databinding.FragmentDetailCategoryBinding
 
 // TODO: Rename parameter arguments, choose names that match
@@ -46,6 +47,10 @@ class DetailCategoryFragment : Fragment() {
 
         binding.tvCategoryName.text = dataName
         binding.tvCategoryDescription.text = "Stock : $dataDescription"
+
+        binding.btnHome.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_detailCategoryFragment_to_homeFragment)
+        )
     }
 
     override fun onDestroy() {
